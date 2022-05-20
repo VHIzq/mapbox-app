33004ef44d339845829f33698d43e2a2de1fa647
Victor Hugo <vrmq@gft.com>
2022-05-20 11:16:05 -0500

    feature: add mapbox-module and shows mapp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoidmhpenEiLCJhIjoiY2wzZHZiNXN2MDFueTNqdDF2eG1memUyYyJ9.McIXyB1woP-iA4xRnOzhmA',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapBoxToken: 'pk.eyJ1IjoidmhpenEiLCJhIjoiY2wzZHZiNXN2MDFueTNqdDF2eG1memUyYyJ9.McIXyB1woP-iA4xRnOzhmA',
 };
 
 /*
